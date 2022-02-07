cdcd80f20c84d093162c55327f5be127b5b0db90
eduper1 <eduper.havard@gmail.com>
2022-02-07 16:47:57 +0300

    instansiate frame class with attributes: bg, location=root, width and height

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from tkinter import *
 
 root = Tk()
+
+# Over ride the settings of the windows
 root.configure(bg="black")
 root.geometry('1020x720')
 root.title("Minesweeper Game")
 root.resizable(False, False)
+
+# create game frame
+top_frame = Frame(
+    root,
+    bg='red', # Change later to black
+    width=1020,
+    height=180
+    
+)
+
+# Run the window
 root.mainloop()
