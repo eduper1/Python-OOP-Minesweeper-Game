c395395f9ac6a059c9713a5cdcf5b7c6bed7d590
eduper1 <eduper.havard@gmail.com>
2022-02-08 22:15:51 +0300

    create method for button object

--- cell.py
+++ cell.py
@@ -1,3 +1,12 @@
+from tkinter import Button
 class Cell:
     def __init__(self, is_mine=False):
         self.is_mine = is_mine
+        self.cell_btn_object = None
+    
+    def create_btn_object(self, location):
+        btn = Button(
+            location,
+            text='text'
+        )
+        self.cell_btn_object = btn
