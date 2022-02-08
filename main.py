1985dd8ee8e35189df3b6832a871b87d930f1b30
eduper1 <eduper.havard@gmail.com>
2022-02-08 22:18:00 +0300

    cell1 object created

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from cell import Cell
 import settings
 import Utils
 
@@ -36,6 +37,13 @@ center_frame = Frame(
     height=Utils.height_prct(75)
 )
 center_frame.place(x=Utils.width_prct(25), y=Utils.height_prct(25))
+
+c1 = Cell()
+c1.create_btn_object(center_frame)
+c1.cell_btn_object.place(
+    x=0,
+    y=0
+)
 
 # Run the window
 root.mainloop()
