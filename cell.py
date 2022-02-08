96709ec58d925dc4a7dfe4593b7680d0109a2ef4
eduper1 <eduper.havard@gmail.com>
2022-02-08 23:13:26 +0300

    Created two methods: right_click_actions and left_click_action to triger neccessary event once a button is clicked

--- cell.py
+++ cell.py
@@ -9,4 +9,16 @@ class Cell:
             location,
             text='text'
         )
+        # left click
+        btn.bind('<Button-1>', self.left_click_actions) # event trigger, DON"T call the method, just pass it as a reference
+        # right click
+        btn.bind('<Button-3>', self.right_click_actions)
         self.cell_btn_object = btn
+    
+    def left_click_actions(self, event):
+        print(event)
+        print('I am left clicked')
+        
+    def right_click_actions(self, event):
+        print(event)
+        print('I am right clicked')
