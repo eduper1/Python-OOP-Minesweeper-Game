f75170638a6ef059b0839c7671dd4daef910bcaf
eduper1 <eduper.havard@gmail.com>
2022-02-10 20:04:35 +0300

    display total number of mine cells that surrounds a particular cell

--- cell.py
+++ cell.py
@@ -21,7 +21,7 @@ class Cell:
             location,
             width=12,
             height=4,
-            text=f'{self.x}, {self.y}'
+            #text=f'{self.x}, {self.y}'
         )
         # left click
         # event trigger, DON"T call the method, just pass it as a reference
@@ -65,7 +65,9 @@ class Cell:
         return counter
     
     def show_cell(self):
-        print(self.surrounded_cells_mines_length)
+        self.cell_btn_object.configure(
+            text=self.surrounded_cells_mines_length
+        )
 
     def show_mine(self):
         # a logic to interrupt the game and display a message that the player lost
