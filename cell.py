01f836e91c56ab78c18d69208386af439112992c
eduper1 <eduper.havard@gmail.com>
2022-02-11 20:45:35 +0300

    Added a  condition when 0 zero mine surrounded cell is clikced

--- cell.py
+++ cell.py
@@ -34,6 +34,9 @@ class Cell:
         if self.is_mine:
             self.show_mine()
         else:
+            if self.surrounded_cells_mines_length == 0:
+                for cell_obj in self.surrounded_cells:
+                    cell_obj.show_cell()
             self.show_cell()
 
     def get_cell_by_axis(self, x, y):
