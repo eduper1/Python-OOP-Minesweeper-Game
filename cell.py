87a90f362d08955979f103e450eac8dd33c5664c
eduper1 <eduper.havard@gmail.com>
2022-02-11 20:27:15 +0300

    Error solved

--- cell.py
+++ cell.py
@@ -47,9 +47,10 @@ class Cell:
             self.get_cell_by_axis(self.x + 1, self.y),
             self.get_cell_by_axis(self.x - 1, self.y),
             self.get_cell_by_axis(self.x, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
             self.get_cell_by_axis(self.x + 1, self.y + 1),
             self.get_cell_by_axis(self.x - 1, self.y + 1),
-            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            #self.get_cell_by_axis(self.x - 1, self.y + 1),
             self.get_cell_by_axis(self.x - 1, self.y - 1),
             self.get_cell_by_axis(self.x + 1, self.y - 1), 
         ]
